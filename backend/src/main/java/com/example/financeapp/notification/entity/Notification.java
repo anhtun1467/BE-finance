7831ee4a11e441acc95553043930ab30145c4230
tuanburh1134 <tuan75035@gmail.com>
2025-12-16 03:43:13 +0700

    fix conflic

--- backend/src/main/java/com/example/financeapp/notification/entity/Notification.java
+++ backend/src/main/java/com/example/financeapp/notification/entity/Notification.java
@@ -69,6 +69,7 @@ public class Notification {
         WALLET_MEMBER_REMOVED,  // User nhận: bị xóa khỏi ví bởi chủ ví
         WALLET_DELETED,         // User nhận: ví đã bị xóa bởi chủ ví
         WALLET_MERGED,          // User nhận: ví của mình đã được gộp với ví khác
+        WALLET_TRANSACTION,     // User nhận: có giao dịch mới trong ví được chia sẻ
         SCHEDULED_TRANSACTION_FAILED,   // User nhận: giao dịch tự động thất bại (không đủ số dư)
         SCHEDULED_TRANSACTION_SUCCESS,  // User nhận: giao dịch tự động thành công
         SYSTEM_ANNOUNCEMENT     // Thông báo hệ thống
